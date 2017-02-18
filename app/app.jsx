489def0c6b7b66fd144abdea448a3c5a29becebf
Nahid <nahidshaiket10300@gmail.com>
2017-02-18 12:59:06 +0600

    successfully setup project

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var TodoApp = require('TodoApp');
 
 //Load Foundation
 // $(document).foundation();
@@ -9,6 +10,8 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-  <p>Boilerplate project</p>,
+  <div>
+      <TodoApp />
+  </div>,
   document.getElementById('app')
 );
