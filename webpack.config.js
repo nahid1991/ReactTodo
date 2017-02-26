160422cd2b8a6b1cf68a03e9a244313f80d7fd98
Nahid <nahidshaiket10300@gmail.com>
2017-02-26 14:11:53 +0600

    localstorage work

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,8 @@ module.exports = {
     root: __dirname,
     modulesDirectories: [
       'node_modules',
-      './app/components'
+      './app/components',
+      './app/api'
     ],
     alias: {
       applicationStyles: 'app/styles/app.scss'
