82ffa4924b7ce5b68a570f1cab3cccb396d3d701
Nahid <nahidshaiket10300@gmail.com>
2017-03-01 11:46:34 +0600

    random

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -61,7 +61,7 @@ var TodoApp = React.createClass({
 
     return (
       <div>
-        <h1 className="page-title">Todo App</h1>
+        <h1 className="page-title">Todo App(React)</h1>
         <div className="row">
           <div className="column small-centered small-11 medium-6 large-5">
             <div className="container">
