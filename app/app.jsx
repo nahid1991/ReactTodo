eaa3a70314eaf880ec192d20531e31a5d003ea59
Nahid <nahidshaiket10300@gmail.com>
2017-03-10 01:01:19 +0600

    fetching data from firebase

--- app/app.jsx
+++ app/app.jsx
@@ -9,14 +9,16 @@ var store = require('configureStore').configure();
 var TodoAPI = require('TodoAPI');
 
 
-store.subscribe(() => {
-  var state = store.getState();
-  console.log('New state', state);
-  TodoAPI.setTodos(state.todos);
-});
+// store.subscribe(() => {
+//   var state = store.getState();
+//   console.log('New state', state);
+//   TodoAPI.setTodos(state.todos);
+// });
 
-var initialTodos = TodoAPI.getTodos();
-store.dispatch(actions.addTodos(initialTodos));
+// var initialTodos = TodoAPI.getTodos();
+// store.dispatch(actions.addTodos(initialTodos));
+
+store.dispatch(actions.startAddTodos());
 
 // store.dispatch(actions.addTodo('Clean the yard'));
 // store.dispatch(actions.setSearchText('yard'));
