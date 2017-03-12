1836a399017daae9166b5a82fd17899080b79c7b
Nahid <nahidshaiket10300@gmail.com>
2017-03-12 23:26:22 +0600

    bundle js fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var webpack = require('webpack');
 var path = require('path');
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 module.exports = {
   entry: [
     'script!jquery/dist/jquery.min.js',
@@ -16,6 +18,11 @@ module.exports = {
     new webpack.ProvidePlugin({
       '$': 'jquery',
       'jQuery': 'jquery'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor : {
+        warnings: false
+      }
     })
   ],
 
@@ -56,7 +63,7 @@ module.exports = {
       path.resolve(__dirname, './node_modules/foundation-sites/scss')
     ]
   },
-  devtool: 'inline-source-map',
+  devtool: process.env.NODE_ENV === 'production' ? undefined : 'inline-source-map',
   devServer: {
       historyApiFallback: true
   }
