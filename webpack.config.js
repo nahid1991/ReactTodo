6573ff7473420feeed2d4e9ba43829ad0ef14153
Nahid <nahidshaiket10300@gmail.com>
2017-03-13 13:04:09 +0600

    for heroku

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
       }
     }),
     new webpack.DefinePlugin({
-      'process_env' : {
+      'process.env' : {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
         API_KEY: JSON.stringify(process.env.API_KEY),
         AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
